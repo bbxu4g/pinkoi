0454de5c8c0f2def6cbb8885ff5fed0752e30b33
bbxu4g <52648679+bbxu4g@users.noreply.github.com>
2019-10-01 19:37:06 +0800

    btn click

--- js/JStry5.js
+++ js/JStry5.js
@@ -1,12 +1,8 @@
 $(function () {
-    var j = $(".small-pic>img").length;
-    var showImage = $('#show-img');
-    $('.small-pic img').click(function () {
-        var i = $(this).attr("id").substr(0);
+    var value = $("input").val();
 
-        showImage.attr('src', "./" + i + ".JPG");
-    })
-    // console.log($(".small-pic>img").length);
+
+
 
     $.ajax({
         url: "../data/test.json",  //要讀取的檔案路徑
@@ -19,7 +15,7 @@ $(function () {
                 $(".detail").append("  <div class='card-deck' style='width:200px;margin:5px;float:left;'><div class='card mx-1' style='border-style:none;'>" +
                     " <img class='card-img-top' src='" + data.first[i].pic + "' alt='Card image cap'> <div class='card-body px-0 py-1'>" +
                     " <p class='card-text' style='font-size: 14px;font-weight: bold;'>" + data.first[i].h4 + "</h4>" +
-                    "<p class='card-text'><small class='text-muted' style='color:#48a38e;'>" + data.first[i].p + "</small></p><input type='number'>　<button class='btn-primary'>購物車</button></div></div>");
+                    "<p class='card-text'><small class='text-muted' style='color:#48a38e;'>" + data.first[i].p + "</small></p><input type='number'>　<button class='btn-primary add'>加入購物車</button></div></div>");
                 //       console.log(data.first.length);
             }
         },
@@ -27,7 +23,7 @@ $(function () {
             alert("ERROR!!!");
         }
     });
-    
+
     $.ajax({
         url: "../data/test2.json",  //要讀取的檔案路徑
         data: {},
@@ -39,20 +35,23 @@ $(function () {
                 $(".detail2").append("  <div class='card-deck' style='width:200px;margin:5px;float:left;'><div class='card mx-1' style='border-style:none;'>" +
                     " <img class='card-img-top' src='" + data.first[i].pic + "' alt='Card image cap'> <div class='card-body px-0 py-1'>" +
                     " <p class='card-text' style='font-size: 14px;font-weight: bold;'>" + data.first[i].h4 + "</h4>" +
-                    "<p class='card-text'><small class='text-muted' style='color:#48a38e;'>" + data.first[i].p + "</small></p></div></div>");
+                    "<p class='card-text'><small class='text-muted' style='color:#48a38e;'>" + data.first[i].p + "</small></p><input type='number'>　<button class='btn-primary Add'>加入購物車</button></div></div>");
                 //       console.log(data.first.length);
             }
+            addcart()
         },
         error: function () { //錯誤的時候執行
             alert("ERROR!!!");
         }
     });
-    //ul滑過去會出現li底下的ul
-    $(".list-ul>li").hover(function () { $(this).find("ul").css("display", "block"); },
-        function () { $(this).find("ul").css("display", "none"); });
-
 
 });
 
-
+function addcart() {
+    $(".add").click(function () {
+        console.log("AA");
+        $(this).closest("input").val();
+        // console.log($(this).closest("input").val());
+    })
+}
 
