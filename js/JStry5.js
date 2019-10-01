1ba0d403681fefda1f99ee4c7ed4331ac127b06d
bbxu4g <52648679+bbxu4g@users.noreply.github.com>
2019-10-01 20:29:30 +0800

    console.log($("small").text());

--- js/JStry5.js
+++ js/JStry5.js
@@ -1,5 +1,5 @@
 $(function () {
-    var value = $("input").val();
+
 
 
 
@@ -34,7 +34,7 @@ $(function () {
             for (var i = 0; i < data.first.length; i++) {
                 $(".detail2").append("  <div class='card-deck' style='width:200px;margin:5px;float:left;'><div class='card mx-1' style='border-style:none;'>" +
                     " <img class='card-img-top' src='" + data.first[i].pic + "' alt='Card image cap'> <div class='card-body px-0 py-1'>" +
-                    " <p class='card-text' style='font-size: 14px;font-weight: bold;'>" + data.first[i].h4 + "</h4>" +
+                    " <p class='card-text' style='font-size: 14px;font-weight: bold;'>" + data.first[i].h4 + "</p>" +
                     "<p class='card-text'><small class='text-muted' style='color:#48a38e;'>" + data.first[i].p + "</small></p><input type='number'>　<button class='btn-primary Add'>加入購物車</button></div></div>");
                 //       console.log(data.first.length);
             }
@@ -48,10 +48,12 @@ $(function () {
 });
 
 function addcart() {
+    var list = [];
+    var num;
     $(".add").click(function () {
-        console.log("AA");
-        $(this).closest("input").val();
-        // console.log($(this).closest("input").val());
+        num = $(this).prev("input").val();//找出同一層的上一個
+        console.log($(this).prev("small").text());
+        list.push(num);
     })
 }
 
